ae3e7195ac7f99302f9047ccf6a1a379fcee8da3
Virtual Connect Group <kyle@virtualconnectgroup.com>
2022-02-20 17:19:48 -0800

    Heart Loading No Logic

--- src/client/client.ts
+++ src/client/client.ts
@@ -26,14 +26,14 @@ const texturesList = [
 
 let loader = new THREE.TextureLoader();
 
-let texture = new THREE.TextureLoader().load('https://augmentyourworldimages.s3.amazonaws.com/LoveCoupons-G/7GHV-Valentines_Coupons_6.png')
+let randTexture = new THREE.TextureLoader().load('https://augmentyourworldimages.s3.amazonaws.com/LoveCoupons-G/7GHV-Valentines_Coupons_6.png')
 
 
 let randIndex = THREE.MathUtils.randInt(0,texturesList.length -1);
-let randTexture = loader.load(texturesList[randIndex]);
+
 let circleGeometry = new THREE.CircleGeometry(1, 100);
 let meshMaterial = new THREE.MeshBasicMaterial({
-  map: texture,
+  map: randTexture,
   transparent: true,
   opacity: 1,
 });
@@ -64,3 +64,5 @@ function render() {
     renderer.render(scene, camera)
 }
 animate()
+
+
